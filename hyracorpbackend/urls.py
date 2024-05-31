fcbdf684122296e72065d259f948ad6ecef5f333
darkwebplayer <amrithanadhm@gmail.com>
2024-05-31 17:26:59 +0530

    fixed urls

--- hyracorpbackend/urls.py
+++ hyracorpbackend/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/auth/", include("user_auth.urls")),
-    path("api/v1/profile/", include("user_profile.urls")),
 ]
