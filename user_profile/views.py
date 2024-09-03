c4bd9f044ebcef59ff378bb453a1e61898e3856b
darkwebplayer <amrithanadhm@gmail.com>
2024-09-03 06:36:33 +0530

    updated profile view to return user basic details

--- user_profile/views.py
+++ user_profile/views.py
@@ -14,12 +14,17 @@ class TenantUserProfileView(APIView):
     def get(self, request):
         user = request.user
         tenant_profile = TenantUserProfile.objects.filter(user=user).first()
+        user_data = {"id": user.id, "email": user.email,
+                     "first_name": user.first_name, "last_name": user.last_name}
         if tenant_profile:
             serializer = TenantUserProfileSerializer(tenant_profile)
-            return Response(serializer.data)
+            data = serializer.data
+            data["user_data"] = user_data
+            return Response({"data": data}, status=status.HTTP_200_OK)
         else:
             return Response(
-                data={"message": "Profile not found, pls update"},
+                data={"message": "Profile not found, pls update",
+                      user: user_data},
                 status=status.HTTP_404_NOT_FOUND,
             )
 
@@ -57,12 +62,17 @@ class LandlordUserProfileView(APIView):
         user = request.user
         landlord_profile = LandlordUserProfile.objects.filter(
             user=user).first()
+        user_data = {"id": user.id, "email": user.email,
+                     "first_name": user.first_name, "last_name": user.last_name}
         if landlord_profile:
             serializer = LandlordUserProfileSerializer(landlord_profile)
-            return Response(serializer.data)
+            data = serializer.data
+            data["user_data"] = user_data
+            return Response({"data": data}, status=status.HTTP_200_OK)
         else:
             return Response(
-                data={"message": "Profile not found, pls update"},
+                data={"message": "Profile not found, pls update",
+                      user: user_data},
                 status=status.HTTP_404_NOT_FOUND,
             )
 
